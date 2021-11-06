25fc65b5f3bb8c1757df24f4f8cc49a0f610a168
Andrei Punko <andd3dfx@gmail.com>
2021-11-06 15:35:36 +0300

    Add absent test for mapper. Adjust JaCoCO exclusions

--- src/main/java/by/andd3dfx/templateapp/dto/ArticleDto.java
+++ src/main/java/by/andd3dfx/templateapp/dto/ArticleDto.java
@@ -9,10 +9,14 @@ import java.time.LocalDateTime;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Data
 @JsonInclude(Include.NON_NULL)
